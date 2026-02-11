cfd79bb2a0f59a16666521e1f65abfa05cb91ea3
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-11 22:16:51 +0800

    init project

---
+++ KeynoteLastLetterAXBug/KeynoteLastLetterAXBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeynoteLastLetterAXBugApp.swift
+//  KeynoteLastLetterAXBug
+//
+//  Created by Guillaume Leclerc on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct KeynoteLastLetterAXBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
